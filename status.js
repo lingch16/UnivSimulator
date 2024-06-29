2c313ce6559198a65ef96ccd0989bc1ba3427ceb
lingch16 <68737603+lingch16@users.noreply.github.com>
2024-06-29 17:46:41 +0800

    Update status.js

--- status.js
+++ status.js
@@ -5,16 +5,43 @@ const statusBanner = {
     moodSpan: document.createElement("span"),
     spiritSpan: document.createElement("span"),
     moneySpan: document.createElement("span"),
+    strspan1: document.createElement("span"),
+    strspan2: document.createElement("span"),
+    moospan1: document.createElement("span"),
+    moospan2: document.createElement("span"),
+    spispan1: document.createElement("span"),
+    spispan2: document.createElement("span"),
+    monspan1: document.createElement("span"),
+    monspan2: document.createElement("span"),
     strT: "体力：",
     mooT: "心情：",
     spiT: "精力：",
     monT: "金钱：",
     createDiv: function(player) {
         //初始化
-        this.strengthSpan.textContent = this.strT + player.strength.toString();
-        this.moodSpan.textContent = this.mooT + player.mood.toString();
-        this.spiritSpan.textContent = this.spiT + player.spirit.toString();
-        this.moneySpan.textContent = this.monT + player.money.toString();
+        this.strspan1.textContent = this.strT;
+        this.moospan1.textContent = this.mooT;
+        this.spispan1.textContent = this.spiT;
+        this.monspan1.textContent = this.monT;
+        
+        this.strspan2.textContent = player.strength.toString();
+        this.moospan2.textContent = player.mood.toString();
+        this.spispan2.textContent = player.spirit.toString();
+        this.monspan2.textContent = player.money.toString();
+
+        this.strengthSpan.appendChild(this.strspan1);
+        this.strengthSpan.appendChild(this.strspan2);
+        this.moodSpan.appendChild(this.moospan1);
+        this.moodSpan.appendChild(this.moospan2);
+        this.spiritSpan.appendChild(this.spispan1);
+        this.spiritSpan.appendChild(this.spispan2);
+        this.moneySpan.appendChild(this.monspan1);
+        this.moneySpan.appendChild(this.monspan2);
+
+        this.strengthSpan.className = "status-span";
+        this.moodSpan.className = "status-span";
+        this.spiritSpan.className = "status-span";
+        this.moneySpan.className = "status-span";
 
         let div = document.createElement("div");
         div.appendChild(this.strengthSpan);
@@ -26,9 +53,30 @@ const statusBanner = {
         return div;
     },
     updateDiv: function(player) {
-        this.strengthSpan.textContent = this.strT + player.strength.toString();
-        this.moodSpan.textContent = this.mooT + player.mood.toString();
-        this.spiritSpan.textContent = this.spiT + player.spirit.toString();
-        this.moneySpan.textContent = this.monT + player.money.toString();
+        this.strspan2.textContent = player.strength.toString();
+        this.moospan2.textContent = player.mood.toString();
+        this.spispan2.textContent = player.spirit.toString();
+        this.monspan2.textContent = player.money.toString();
+        if (player.strength < 10) {
+            this.strspan2.className = "warning-status-span";
+        } else {
+            this.strspan2.className = "plain-status-span";
+        }
+        if (player.mood < 10) {
+            this.moospan2.className = "warning-status-span";
+        } else {
+            this.moospan2.className = "plain-status-span";
+        }
+        if (player.spirit < 10) {
+            this.spispan2.className = "warning-status-span";
+        } else {
+            this.spispan2.className = "plain-status-span";
+        }
+        if (player.money < 500) {
+            this.monspan2.className = "warning-status-span";
+        } else {
+            this.monspan2.className = "plain-status-span";
+        }
+
     }
 }
