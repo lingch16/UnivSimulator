045ce693f5c92ca46815135e76e9a6c851bd716c
lingch16 <68737603+lingch16@users.noreply.github.com>
2024-07-03 19:16:53 +0800

    Update univ.js

--- univ.js
+++ univ.js
@@ -37,7 +37,7 @@ const rank3Univ = new Univ("浙江的大学", "简称浙大，top3大学，省�
 const rank4Univ = new Univ("南京的大学", "简称南大，学风正，当之无愧的第一梯队。", 4, 1, 4, 3, 3, 3, 4, 6000, 0, 0);
 const rank5Univ = new Univ("某财经大学", "社科类专业实力强劲。", 5, 2, 2, 3, 3, 2, 3, 8000, socialMajorCode, 1);
 const rank6Univ = new Univ("某科技大学", "只有科学没有技术，理科实力强劲。", 6, 2, 3, 2, 2, 3, 4, 6500, scienceMajorCode, 1);
-const rank7Univ = new Univ("某医科大学", "学校的附属医院近期成功晋升三甲综合医院。", 7, 2, 4, 2, 1, 3, 4, 6500, medicalMajorCode, 1);
+const rank7Univ = new Univ("某医科大学", "学校的附属医院近期成功晋升三甲综合医院。", 7, 2, 3, 2, 1, 3, 4, 6500, medicalMajorCode, 1);
 const rank8Univ = new Univ("某师范大学", "学校优先发展纯文科，人文大类是业界翘楚。", 8, 2, 2, 2, 2, 2, 3, 7000, artMajorCode, 1);
 const rank9Univ = new Univ("某工业大学", "学校以工科起家，近期在向新工科转型。", 9, 2, 2, 2, 2, 3, 4, 7000, engineerMajorCode, 1);
 const rank10Univ = new Univ("中等偏上的大学", "比较均衡，学费也是中等偏上的。", 10, 3, 2, 2, 2, 2, 2, 8000, 0, 0);
@@ -48,21 +48,12 @@ const rankexUniv = new Univ("辣鸡大学", "不如不上的烂学校", 20, 5, 0
 
 const goodUniv = [rank1Univ, rank2Univ, rank3Univ, rank4Univ];
 const fairUniv = [rank5Univ, rank6Univ, rank7Univ, rank8Univ, rank9Univ];
-const poorUniv = [rank10Univ, rank11Univ, rank12Univ];
-
-const fairRandom =[rank5Univ, rank6Univ, rank7Univ, rank8Univ, rank9Univ];
-fairRandom.splice(GRI(0,4),1);
-
-const goodUnivList = [rank1Univ, rank2Univ, rank3Univ, rank4Univ, fairUniv[GRI(0, 4)]];
-const fairUnivList = [fairRandom[0], fairRandom[1], fairRandom[2], fairRandom[3], poorUniv[GRI(0, 2)]];
-const poorUnivList = [fairUniv[GRI(0, 4)], rank10Univ, rank11Univ, rank12Univ, rank13Univ];
+const poorUniv = [rank10Univ, rank11Univ, rank12Univ, rank13Univ];
+//为防止未知bug出现，把以上数组复制一遍
+const goodUnivList = [rank1Univ, rank2Univ, rank3Univ, rank4Univ];
+const fairUnivList = [rank5Univ, rank6Univ, rank7Univ, rank8Univ, rank9Univ];
+const poorUnivList = [rank10Univ, rank11Univ, rank12Univ, rank13Univ];
 
 const univList = [rank1Univ, rank2Univ, rank3Univ, rank4Univ, rank5Univ, rank6Univ, rank7Univ, rank8Univ, rank9Univ, rank10Univ, rank11Univ, rank12Univ, rank13Univ, rankexUniv];
 
-//随机数方法
-function GRI(min, max) {
-    if (min >= max) {
-        return max;
-    }  //这个判断可能是多余的
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
